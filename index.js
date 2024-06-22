94d61c27243cfeab12f7ec839bee30930f41d4fc
--Explosion-- <explosion.scratch@gmail.com>
2024-06-22 16:48:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,71 @@ const path = require("path");
 const os = require("os");
 
 let tmpdir;
+
+const HELP = `install`;
 async function main() {
+  const log = {
+    debug: (message) =>
+      console.log(`${colors.fg.cyan}${message}${colors.reset}`),
+    log: (message) =>
+      console.log(`${colors.fg.white}${message}${colors.reset}`),
+    error: (message) =>
+      console.error(`${colors.fg.red}${message}${colors.reset}`),
+    warn: (message) =>
+      console.warn(`${colors.fg.yellow}${message}${colors.reset}`),
+  };
+
   const args = process.argv.slice(2);
+  if (["-h", "--help"].includes(args[0])) {
+    return log.log(HELP);
+  }
+  if (!args[0]) {
+    return log.log(HELP);
+  }
   let isFile = fs.existsSync(args[0]);
+  let isURL =
+    /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(
+      args[0]
+    );
   let filepath = isFile ? path.resolve(args[0]) : null;
 
   if (isFile) {
-    console.log("Installing " + filepath + " from local files");
+    log.debug("Installing " + filepath + " from local files");
   }
 
   // Declare some stuff
   tmpdir = execSync("mktemp -d").toString().trim();
+
+  if (isURL && !args[0].includes("://github.com")) {
+    log.debug("Downloading file from URL...");
+    let response = await fetch(args[0]);
+    const disposition = response.headers.get("Content-Disposition");
+    const filenameRegex = /filename="?([^;"]+)"/;
+    const match = disposition && disposition.match(filenameRegex);
+    let filename =
+      match?.[1] || args[0].substring(args[0].lastIndexOf("/") + 1);
+    const dest = path.join(tmpdir, filename);
+    await response
+      .arrayBuffer()
+      .then((ab) => fs.writeFileSync(dest, Buffer.from(ab)));
+    log.debug("Downloaded " + filename);
+    isFile = true;
+    filepath = dest;
+  }
+
   let selected;
 
   // OS detection stuff:
   const arch = process.arch;
   const platform = process.platform;
-  console.log(`Detected ${arch} on ${platform}`);
+  log.debug(`Detected ${arch} on ${platform}`);
   let arch_aliases = {
     arm64: ["arm64", "arm", "aarch", "aarh64"],
     x64: ["x64", "intel"],
     universal: ["universal", "all"],
   };
   let platform_aliases = {
-    darwin: ["darwin", "osx", "mac", "apple"],
+    darwin: ["darwin", "osx", "macos", "mac", "apple"],
     linux: ["linux"],
     freebsd: ["freebsd", "linux"],
     openbsd: ["openbsd", "linux"],
@@ -53,10 +94,17 @@ async function main() {
       return false;
     }
   };
+
   let canInstall = {
+    // Regular linux
     deb: process.platform === "linux" && ebool("which apt").length > 0,
+
+    // Mac
     dmg: process.platform === "darwin",
+    pkg: process.platform === "darwin",
     app: process.platform === "darwin",
+
+    // Arch linux
     rpm: ebool("which dnf").length > 0,
     "tar.zst": ebool("which pacman").length > 0,
   };
@@ -77,7 +125,14 @@ async function main() {
     ".keystone_install",
     ".VolumeIcon.icns",
     ".DS_Store",
+    "CHANGELOG",
+    "LICENSE",
   ];
+
+  const isIgnored = (filename) => {
+    return IGNORE.find((p) => filename.toLowerCase().includes(p.toLowerCase()));
+  };
+
   if (!isFile) {
     const [owner, repo] = args[0]
       .replace(/(?:https?\:\/\/)?github\.com\//, "")
@@ -91,12 +146,16 @@ async function main() {
       .then((a) => ((body = a.body), a))
       .then((j) => j.assets)
       .then((assets) => {
-        return assets.filter(
-          (asset) => !IGNORE.find((i) => asset.name.includes(i))
-        );
+        if (!assets?.length) {
+          throw new Error("No assets in releases");
+        }
+        return assets.filter((asset) => !isIgnored(asset.name));
       });
 
-    assets = assets.map((i) => ({ ...i, segments: i.name.split(/[_ \.\-]/) }));
+    assets = assets.map((i) => ({
+      ...i,
+      segments: i.name.split(/[_ \.\-]/).map((j) => j.toLowerCase()),
+    }));
     assets = assets.map((i) => ({
       ...i,
       compatiblePlatforms: i.segments.filter((j) =>
@@ -116,20 +175,20 @@ async function main() {
       code.includes(".sh");
 
     if (code && isInstaller(code)) {
-      console.log("Found installer code in release notes:\n\n" + code + "\n\n");
+      log.debug("Found installer code in release notes:\n\n" + code + "\n\n");
       if (
         await confirm(
           "Run install script (y) or continue to regular installation (n)?"
         )
       ) {
-        console.log("Running...");
+        log.debug("Running...");
         execSync(code);
         return;
       }
     }
     let installerScript = assets.find((i) => i.name.includes("installer.sh"));
     if (installerScript) {
-      console.log(`Found installer script: ${installerScript.name}`);
+      log.debug(`Found installer script: ${installerScript.name}`);
       await fetch(installerScript.browser_download_url)
         .then((r) => r.arrayBuffer())
         .then((ab) =>
@@ -171,7 +230,7 @@ async function main() {
       .filter((i) => !(i.delete == true))
       .sort((a, b) => b.points - a.points);
     if (!compat?.length) {
-      return console.log(
+      return log.error(
         "Couldn't find a binary, you'll have to figure it out yourself 🤷:\n" +
           `https://github.com/${owner}/${repo}`
       );
@@ -185,21 +244,26 @@ async function main() {
     };
   }
   if (!selected) {
-    return console.log("Couldn't find anything to install");
+    return log.error("Couldn't find anything to install");
   }
   const shouldInstall = await confirm(
     `Ok to install ${selected.name} (${fileSize(selected.size)})?`
   );
   if (!shouldInstall) {
-    return console.log("Aborted install");
+    return log.error("Aborted install");
   }
   process.chdir(tmpdir);
-  console.log(`Downloading ${selected.name}`);
+  log.debug(`Downloading ${selected.name}`);
 
   const checkPath = async (p) => {
     if (fs.existsSync(path.resolve(p))) {
       if (!(await confirm(`Overwrite ${p}?`))) {
         throw new Error("Aborted overwrite of file " + p);
+      } else {
+        try {
+          log.debug("Removing " + p);
+          fs.rmSync(p, { recursive: true, force: true });
+        } catch (e) {}
       }
     }
   };
@@ -207,6 +271,7 @@ async function main() {
   const getName = (sel) =>
     sel.name
       .replace(sel.extension, "")
+      .replace(/v?[0-9]+\.[0-9]+\.[0-9]+/i, "")
       .replace(
         new RegExp(
           `(?:${[...possible.platforms, ...possible.arches].join("|")})`,
@@ -214,11 +279,10 @@ async function main() {
         ),
         ""
       )
-      .replace(/v?[0-9]+\.[0-9]+\.[0-9]+/i, "")
       .replace(/[ _\-\.]+$/, "")
       .replace(/^[ _\-\.]+/, "");
   const mountDMG = (p) => {
-    console.log("Mounting DMG", p);
+    log.debug("Mounting DMG", p);
     execSync(
       `hdiutil attach ${JSON.stringify(
         p
@@ -226,53 +290,59 @@ async function main() {
     );
   };
   const installPkg = (p) => {
-    console.log("Installing pkg...");
+    if (!canInstall.pkg) {
+      throw new Error("Can't install .pkg files in this environment");
+    }
+    log.debug("Installing pkg...");
     execSync(`sudo installer -pkg ${JSON.stringify(p)} -target /`);
-    console.log(`Installed pkg ${path.basename(p)} to /`);
+    log.debug(`Installed pkg ${path.basename(p)} to /`);
   };
-  const newpath = path.resolve(tmpdir, path.basename(filepath));
-  fs.cpSync(filepath, newpath);
+  let newpath;
+  if (isFile) {
+    newpath = path.resolve(tmpdir, path.basename(filepath));
+    if (filepath !== newpath) {
+      fs.cpSync(filepath, newpath);
+    }
+  }
   const p = newpath || getPath(selected.name);
   if (!isFile) {
     await fetch(selected.browser_download_url)
       .then((r) => r.arrayBuffer())
-      .then((a) => (console.log(`Downloaded, writing to ${p}`), a))
+      .then((a) => (log.debug(`Downloaded, writing to ${p}`), a))
       .then((b) => fs.writeFileSync(p, Buffer.from(b)));
   }
 
-  console.log(`Preinstall: ${selected.name}`);
+  log.log(`Preinstall: ${selected.name}`);
   const OUTPUT_DIR = path.join(tmpdir, "outputs");
   fs.mkdirSync(OUTPUT_DIR);
 
   let isBinary = true;
   if (selected.extension === "tar.gz") {
-    console.log("Unzipping TAR");
+    log.debug("Unzipping TAR");
     execSync(
       `tar -xzf ${JSON.stringify(p)} --directory ${JSON.stringify(OUTPUT_DIR)}`
     );
   } else if (selected.extension === "pkg") {
     installPkg(p);
   } else if (selected.extension === "zip") {
-    console.log("Unzipping ZIP");
+    log.debug("Unzipping ZIP");
     execSync(`unzip ${JSON.stringify(p)} -d ${JSON.stringify(OUTPUT_DIR)}`);
   } else if (selected.extension === "dmg") {
     isBinary = false;
-    console.log("Mounting DMG");
     mountDMG(p);
   } else if (selected.extension === "deb") {
     isBinary = false;
   } else if (!selected.extension) {
-    console.log("Already a binary");
     fs.renameSync(p, path.join(OUTPUT_DIR, selected.name));
   }
-  let binaries = fs.readdirSync(OUTPUT_DIR).filter((i) => !IGNORE.includes(i));
+  let binaries = fs.readdirSync(OUTPUT_DIR).filter((i) => !isIgnored(i));
   const _dmg = binaries.find((f) => f.endsWith(".dmg"));
   if (_dmg) {
     isBinary = false;
     mountDMG(path.resolve(OUTPUT_DIR, _dmg));
   }
   // Do after possible DMG mounted
-  binaries = fs.readdirSync(OUTPUT_DIR).filter((i) => !IGNORE.includes(i));
+  binaries = fs.readdirSync(OUTPUT_DIR).filter((i) => !isIgnored(i));
   const _app = binaries.find((f) => f.endsWith(".app"));
   const _pkg = binaries.find((f) => f.endsWith(".pkg"));
   if (
@@ -282,35 +352,44 @@ async function main() {
       }?`
     ))
   ) {
-    return console.log("Aborted");
+    return log.error("Aborted");
   }
   const installApp = async (_app) => {
     if (!canInstall.app) {
-      return console.log("Cannot install .app package");
+      return log.error("Cannot install .app package");
     }
-    console.log("Found mac .app package, installing...");
+    log.debug("Found mac .app package, installing...");
     const dest = path.join("/Applications", _app);
     await checkPath(dest);
+    log.debug("Writing files...");
     fs.cpSync(path.join(OUTPUT_DIR, _app), dest, {
       recursive: true,
     });
-    console.log("Code signing app...");
+    log.debug("Code signing app...");
     try {
-      execSync(`codesign --sign - --force --deep ${JSON.stringify(dest)}`);
+      execSync(
+        `codesign --sign - --force --deep ${JSON.stringify(dest)} 2> /dev/null`
+      );
     } catch (e) {
-      console.log("Codesigning failed");
+      log.warn("Codesigning failed");
     }
-    console.log("Removing quarantine from app...");
+    log.debug("Removing quarantine from app...");
     try {
-      execSync(`xattr -rd com.apple.quarantine ${JSON.stringify(dest)}`);
+      execSync(
+        `xattr -rd com.apple.quarantine ${JSON.stringify(dest)} 2> /dev/null`
+      );
     } catch (e) {
-      console.log("Dequarantining failed");
+      log.warn("Dequarantining failed");
     }
     try {
       execSync(`hdiutil eject ${JSON.stringify(OUTPUT_DIR)} 2> /dev/null`, {
         stdio: false,
       });
     } catch (e) {}
+    execSync("sleep 0.5");
+    if (await confirm("Open app " + path.basename(dest))) {
+      execSync(`open -n ${JSON.stringify(dest)}`);
+    }
   };
 
   if (_app) {
@@ -326,10 +405,10 @@ async function main() {
 
   if (isBinary) {
     if (!binaries.length) {
-      return console.log("No binaries found");
+      return log.error("No binaries found");
     }
-    console.log("Got binaries: ", binaries.join("\n"));
-    console.log("Renaming binaries...");
+    log.debug("Got binaries: ", binaries.join("\n"));
+    log.debug("Renaming binaries...");
     for (const bin of binaries) {
       fs.renameSync(
         path.join(OUTPUT_DIR, bin),
@@ -338,19 +417,19 @@ async function main() {
     }
   }
   if (isBinary) {
-    console.log("Installing binary");
-    for (let bin of fs.readdirSync(OUTPUT_DIR)) {
+    log.debug("Installing binary");
+    for (let bin of fs.readdirSync(OUTPUT_DIR).filter((i) => !isIgnored(i))) {
       const dest = path.join(os.homedir(), ".local", "bin", bin);
       await checkPath(dest);
-      console.log(`Installing binary: "${bin}" -> "${dest}"`);
+      log.debug(`Installing binary: "${bin}" -> "${dest}"`);
       execSync(`chmod +x ${JSON.stringify(path.join(OUTPUT_DIR, bin))}`);
       fs.cpSync(path.join(OUTPUT_DIR, bin), dest);
     }
   } else if (selected.extension === "deb") {
-    console.log("Installing DEB");
+    log.debug("Installing DEB");
     execSync(`dpkg -i ${JSON.stringify(p)} ${JSON.stringify(OUTPUT_DIR)}`);
   }
-  console.log("Done:", binaries.join("\n"));
+  log.log("Done:", binaries.join("\n"));
 }
 
 function fileSize(bytes, si = false, dp = 1) {
@@ -418,25 +497,73 @@ function confirm(question, defaultAnswer = "y") {
       input: process.stdin,
       output: process.stdout,
     });
-    interface.question(`${question} (${defaultAnswer}/n) `, (ans) => {
-      let result =
-        ans.trim().toLowerCase() === "y" || ans.trim().toLowerCase() === "yes";
-      if (defaultAnswer === "y" && ans.trim() === "") result = true;
-      interface.close();
-      resolve(result);
-    });
+    interface.question(
+      `${colors.fg.yellow}${question}${colors.reset} ${colors.fg.blue}(${defaultAnswer}/n)${colors.reset} `,
+      (ans) => {
+        let result =
+          ans.trim().toLowerCase() === "y" ||
+          ans.trim().toLowerCase() === "yes";
+        if (defaultAnswer === "y" && ans.trim() === "") result = true;
+        interface.close();
+        resolve(result);
+      }
+    );
   });
 }
 
-(async () => {
-  try {
-    await main();
-  } catch (e) {
-    console.log("Error: " + e.stack);
-  }
+const colors = {
+  reset: "\x1b[0m",
+  bright: "\x1b[1m",
+  dim: "\x1b[2m",
+  underscore: "\x1b[4m",
+  blink: "\x1b[5m",
+  reverse: "\x1b[7m",
+  hidden: "\x1b[8m",
+
+  fg: {
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
+    gray: "\x1b[90m",
+    crimson: "\x1b[38m", // Scarlet
+  },
+  bg: {
+    black: "\x1b[40m",
+    red: "\x1b[41m",
+    green: "\x1b[42m",
+    yellow: "\x1b[43m",
+    blue: "\x1b[44m",
+    magenta: "\x1b[45m",
+    cyan: "\x1b[46m",
+    white: "\x1b[47m",
+    gray: "\x1b[100m",
+    crimson: "\x1b[48m",
+  },
+};
+
+const cleanup = () => {
   if (tmpdir) {
     console.log("Cleaning up...");
     fs.rmSync(tmpdir, { recursive: true });
     console.log("Done");
   }
+};
+(async () => {
+  process.on("SIGINT", () => {
+    cleanup();
+    process.exit(0);
+  });
+
+  try {
+    await main();
+  } catch (e) {
+    console.log("Error: " + e.stack);
+  }
+
+  cleanup();
 })();
