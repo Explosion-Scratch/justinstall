906fc453af47b4618286d15aaf5c4b16fb95e938
Explosion Implosion <explosion.scratch@gmail.com>
2024-06-23 11:29:41 -0500

    Build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf build
+mkdir build
+
+VERSION=v1.0.0
+bun build --compile --target=bun-linux-x64 index.js --outfile build/justinstall-$VERSION-linux-x64
+bun build --compile --target=bun-linux-arm64 index.js --outfile build/justinstall-$VERSION-linux-arm64
+bun build --compile --target=bun-windows-x64 index.js --outfile build/justinstall-$VERSION-windows-x64
+bun build --compile --target=bun-darwin-x64 index.js --outfile build/justinstall-$VERSION-darwin-x64
+bun build --compile --target=bun-darwin-arm64 index.js --outfile build/justinstall-$VERSION-darwin-arm64
